d399ce575e957a8e0888fda56c2c47b448557ce4
senanweb <senansalawe@gmail.com>
2021-01-14 00:16:48 +0100

    Omar-6

--- Test-Automation/JavaScript/learn/7-loop.js
+++ Test-Automation/JavaScript/learn/7-loop.js
@@ -2,5 +2,5 @@
 var fAmily = ["Nadom","Arkan","Senan","Omar"];
 console.log(fAmily)
 // make loop to print the names
-for (i =0; i<fAmily.length; i++)
+for (i =0; i < fAmily.length; i++)
     {console.log(fAmily[i]);}

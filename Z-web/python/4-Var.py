c6a55c024d7bfc6b202c1ff4719fcfe4ba425e6d
senanweb <senansalawe@gmail.com>
2021-03-07 23:57:05 +0100

    zero-python

---
+++ Z-web/python/4-Var.py
@@ -0,0 +1,4 @@
+name = "omar"
+print (name)
+a,b,c = 1,2,3
+print (a,b,c)
---
+++ Z-web/python/8-Strings-Indexing.py
@@ -0,0 +1,25 @@
+myString = "I Love Python"
+
+print(myString[0])  # Index 0 => I
+print(myString[9])  # Index 9 => t
+print("-------")
+#----------------------------------
+print(myString[-1])  # Index -1 => First Character From End
+print(myString[-6])  # Index -6 => 6th Character From End
+print("-------")
+#----------------------------------
+print(myString[8:11])  # yth
+print(myString[3:5])  # ov
+print("-------")
+#----------------------------------
+print(myString[:10])  # If Start Is Not Here Will Start From 0 (I Love Pyt)
+print(myString[5:])  # If End Is Not Here Will Go To The End (e Python)
+print(myString[:])  # Full Data
+print("-------")
+#----------------------------------
+print(myString[0::1])  # Full Data
+print(myString[::1])  # Full Data
+print("-------")
+#----------------------------------
+print(myString[::2]) # Escape 1 number
+print(myString[::3]) # Escape 2 number

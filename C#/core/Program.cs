6a36bdc4b71c881234a52131262fd392871bb16c
senanweb <senansalawe@gmail.com>
2021-03-21 23:18:45 +0100

    c# use vscode

---
+++ C#/core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bismillah");
+        }
+    }
+}
+/* make project 1- create Folder
+2- access to folder from terminal
+3- write dotnet new console
+4- write your code
+5- run project from terminal
+6- write dotnet build
+7- write dotnet run
+*/
